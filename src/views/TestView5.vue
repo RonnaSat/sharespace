550e4fa1a69bd1ce08eb0b0210b9bb12a33a1b7a
RonnaSat <ronnakornniseansut@gmail.com>
2025-01-22 11:35:40 +0700

    add usable fabric vue in testview5

--- src/views/TestView5.vue
+++ src/views/TestView5.vue
@@ -1,102 +1,64 @@
 <template>
-  <div class="editor-container">
-    <div class="toolbar">
-      <button @click="enableDrawing">Draw</button>
-      <button @click="addRectangle">Rectangle</button>
-      <button @click="addCircle">Circle</button>
-      <input type="color" v-model="currentColor" @change="updateColor" />
-      <input
-        type="range"
-        v-model="brushSize"
-        min="1"
-        max="20"
-        @change="updateBrushSize"
-      />
-    </div>
-    <canvas id="canvas"></canvas>
-  </div>
+  <canvas id="main-canvas" width="800" height="600"></canvas>
+  <table>
+    <tr v-for="box in boxList" :key="box.id">
+      <td>{{ box.get("id") }}</td>
+      <td>{{ box.get("left") }}</td>
+      <td>{{ box.get("description") }}</td>
+    </tr>
+  </table>
+  <button @click="addBox">Add</button>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from "vue";
 import * as fabric from "fabric";
 
-export default {
-  data() {
-    return {
-      canvas: null,
-      currentColor: "#000000",
-      brushSize: 5,
-      isDrawing: false,
-    };
-  },
-  mounted() {
-    this.initCanvas();
-  },
-  methods: {
-    initCanvas() {
-      this.canvas = new fabric.Canvas("canvas", {
-        width: 800,
-        height: 600,
-        backgroundColor: "#ffffff",
-      });
-    },
-    enableDrawing() {
-      this.canvas.isDrawingMode = !this.canvas.isDrawingMode;
-      if (this.canvas.isDrawingMode) {
-        this.canvas.freeDrawingBrush.width = this.brushSize;
-        this.canvas.freeDrawingBrush.color = this.currentColor;
-      }
-    },
-    addRectangle() {
-      const rect = new fabric.Rect({
-        left: 100,
-        top: 100,
-        width: 100,
-        height: 100,
-        fill: this.currentColor,
-        selectable: true, // Make the rectangle selectable
-      });
-      this.canvas.add(rect);
-    },
-    addCircle() {
-      const circle = new fabric.Circle({
-        left: 100,
-        top: 100,
-        radius: 50,
-        fill: this.currentColor,
-        selectable: true, // Make the circle selectable
-      });
-      this.canvas.add(circle);
-    },
-    updateColor() {
-      if (this.canvas.isDrawingMode) {
-        this.canvas.freeDrawingBrush.color = this.currentColor;
-      }
-    },
-    updateBrushSize() {
-      if (this.canvas.isDrawingMode) {
-        this.canvas.freeDrawingBrush.width = parseInt(this.brushSize);
-      }
-    },
-  },
+// Reactive data
+const boxList = ref([]);
+let canvas = null;
+
+class TestRect extends fabric.Rect {
+  constructor(options) {
+    super(options);
+    this.set("id", options.id);
+    this.set("description", "default-description");
+  }
+}
+
+// Lifecycle hook
+onMounted(() => {
+  canvas = new fabric.Canvas("main-canvas");
+  canvas.on("object:modified", (event) => {
+    console.log("modified");
+    event.target.set("description", "new-description");
+  });
+});
+
+// Methods
+const addBox = () => {
+  let new_box = new TestRect({
+    left: 10,
+    top: 10,
+    width: 30,
+    height: 40,
+    fill: "green",
+    id: new Date().toISOString(),
+  });
+
+  canvas.add(new_box);
+  boxList.value.push(new_box);
+  canvas.renderAll();
 };
 </script>
 
-<style scoped>
-.editor-container {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 1rem;
-}
-
-.toolbar {
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-}
-
+<style>
 canvas {
-  border: 1px solid #ccc;
+  border: 1px dotted red;
+}
+
+table tr,
+table td {
+  border: 1px solid black;
 }
 </style>
